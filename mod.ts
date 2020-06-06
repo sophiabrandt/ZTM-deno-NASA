c2d24150bf736b5644cdf9ac011d09998f6cba55
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2020-06-06 16:09:46 +0200

    📦 NEW: first oak application

---
+++ mod.ts
@@ -0,0 +1,14 @@
+import { Application } from 'https://deno.land/x/oak@v5.0.0/mod.ts'
+
+const app = new Application()
+const PORT = 8000
+
+app.use((ctx) => {
+  ctx.response.body = `NASA MISSION CONTROL API`
+})
+
+if (import.meta.main) {
+  await app.listen({
+    port: PORT,
+  })
+}
