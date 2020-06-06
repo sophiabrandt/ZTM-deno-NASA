76d4af83874d3f730529152b8ea7beb2c1fcde58
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2020-06-06 16:18:12 +0200

    👌 IMPROVE: add middlewares

--- mod.ts
+++ mod.ts
@@ -3,6 +3,21 @@ import { Application } from 'https://deno.land/x/oak@v5.0.0/mod.ts'
 const app = new Application()
 const PORT = 8000
 
+// simple request logging middleware
+app.use(async (ctx, next) => {
+  await next()
+  const time = ctx.response.headers.get('X-Response-Time')
+  console.log(`${ctx.request.method} ${ctx.request.url}: ${time}`)
+})
+
+// simple timer logging
+app.use(async (ctx, next) => {
+  const start = Date.now()
+  await next()
+  const delta = Date.now() - start
+  ctx.response.headers.set('X-Response-Time', `${delta}ms`)
+})
+
 app.use((ctx) => {
   ctx.response.body = `NASA MISSION CONTROL API`
 })
