0f4de00555eae07117f4fc05ee103aea87ebe1ee
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2020-06-16 16:15:38 +0200

    🐛 FIX: fix whitelist

--- mod.ts
+++ mod.ts
@@ -27,9 +27,11 @@ app.use(async (ctx) => {
     '/stylesheets/style.css',
     '/images/favicon.png',
   ]
-  await send(ctx, filePath, {
-    root: `${Deno.cwd()}/public`,
-  })
+  if (fileWhitelist.includes(filePath)) {
+    await send(ctx, filePath, {
+      root: `${Deno.cwd()}/public`,
+    })
+  }
 })
 
 app.use((ctx) => {
