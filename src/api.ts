d78190e5b9fc2c0310c1f3b98040e59c2d4f8f31
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2020-06-18 14:31:38 +0200

    📦 NEW: add launches

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,7 @@
 import { Router } from './deps.ts'
 
 import * as planets from './models/planets.ts'
+import * as launches from './models/launches.ts'
 
 const router = new Router()
 
@@ -10,6 +11,17 @@ router.get('/', (ctx) => {
 
 router.get('/planets', (ctx) => {
   ctx.response.body = planets.getAll()
+})
+
+router.get('/launches', (ctx) => {
+  ctx.response.body = launches.getAll()
+})
+
+router.get('/launches/:id', (ctx) => {
+  if (ctx.params?.id) {
+    const launchesList = launches.getOne(Number(ctx.params.id))
+    launchesList ? ctx.response.body = launchesList : ctx.throw(400, 'Launch with that ID does not exist')
+  }
 })
 
 export default router
