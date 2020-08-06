7b53954377e340f040f7ab6886a5d15061e52c7b
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2020-08-06 10:48:12 +0200

    👌 IMPROVE: Update deps

--- src/deps.ts
+++ src/deps.ts
@@ -1,16 +1,16 @@
 // standard library
-export * as log from 'https://deno.land/std@0.61.0/log/mod.ts'
-export * as flags from "https://deno.land/std@0.61.0/flags/mod.ts"
+export * as log from 'https://deno.land/std@0.63.0/log/mod.ts'
+export * as flags from "https://deno.land/std@0.63.0/flags/mod.ts"
 
-export { join } from 'https://deno.land/std@0.61.0/path/mod.ts'
-export { parse } from 'https://deno.land/std@0.61.0/encoding/csv.ts'
-export { BufReader } from 'https://deno.land/std@0.61.0/io/bufio.ts'
+export { join } from 'https://deno.land/std@0.63.0/path/mod.ts'
+export { parse } from 'https://deno.land/std@0.63.0/encoding/csv.ts'
+export { BufReader } from 'https://deno.land/std@0.63.0/io/bufio.ts'
 
 // third party dependencies
 export {
   Application,
   Router,
   send,
-} from 'https://deno.land/x/oak@v6.0.0/mod.ts'
+} from 'https://deno.land/x/oak@v6.0.1/mod.ts'
 
-export { pick, chain } from 'https://unpkg.com/rambda@5.9.0/dist/rambda.esm.js'
+export { pick, chain } from 'https://unpkg.com/rambda@5.11.0/dist/rambda.esm.js'
